8225a9993389910db3fa96fba59889a3608441f5
crckrbrrs <crackedberries@gmail.com>
2026-06-12 10:40:32 +0300

    add speed fade trigger

---
+++ Loenn/triggers/SpeedFadeTrigger.lua
@@ -0,0 +1,12 @@
+return {
+	name = "YaoiHelper/SpeedFadeTrigger",
+	nodeLimits = {1, 1},
+	triggerText = "Speed Fade",
+	placements = {
+		name = "main",
+		data = {
+			lower_bound = 60,
+			upper_bound = 400
+		}
+	}
+}
---
+++ Source/Triggers/SpeedFadeTrigger.cs
@@ -0,0 +1,68 @@
+// a bunch of this is referenced from crystalline
+using System.Linq;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.YaoiHelper.Triggers;
+
+// a bunch of this is from crystalline
+// TODO: add checkboxes for only horizontal or only vertical
+
+[CustomEntity($"{nameof(YaoiHelper)}/{nameof(SpeedFadeTrigger)}")]
+[Tracked(false)]
+[Submodule]
+public sealed class SpeedFadeTrigger : Trigger {
+	private readonly Vector2[] nodes;
+	
+	public Trigger? Trigger;
+	public readonly float LowerBound;
+	public readonly float UpperBound;
+
+    public SpeedFadeTrigger(EntityData data, Vector2 offset) : base(data, offset) {
+		nodes = data.NodesOffset(offset);
+		LowerBound = data.Float("lower_bound");
+		UpperBound = data.Float("upper_bound");
+		
+    }
+
+    public override void Awake(Scene scene) {
+        base.Awake(scene);
+		Trigger = scene.CollideFirst<Trigger>(nodes[0]) ?? scene.Tracker.GetNearestEntity<Trigger>(nodes[0]);
+		Trigger?.Collidable = false;
+    }
+
+    public override void OnEnter(Player player) {
+        base.OnEnter(player);
+		Trigger?.OnEnter(player);
+    }
+
+    public override void OnStay(Player player) {
+        base.OnStay(player);
+		Trigger?.OnStay(player);
+    }
+
+    public override void OnLeave(Player player) {
+        base.OnLeave(player);
+		Trigger?.OnLeave(player);
+    }
+
+	internal static void ApplyHooks() {
+		On.Celeste.Trigger.GetPositionLerp += On_TriggerGetPositionLerp_ApplySpeedFade;
+	}
+
+	internal static void RemoveHooks() {
+		On.Celeste.Trigger.GetPositionLerp -= On_TriggerGetPositionLerp_ApplySpeedFade;
+	}
+
+	public static float On_TriggerGetPositionLerp_ApplySpeedFade(On.Celeste.Trigger.orig_GetPositionLerp orig, Trigger self, Player player, PositionModes mode) {
+		if (self.Scene is not Level level) return orig(self, player, mode);
+		foreach (SpeedFadeTrigger speedFadeTrigger in level.Tracker.GetEntities<SpeedFadeTrigger>().Cast<SpeedFadeTrigger>()) {
+			if (speedFadeTrigger.Trigger == self) {
+				return float.Clamp((player.Speed.Length() - speedFadeTrigger.LowerBound) / (speedFadeTrigger.UpperBound - speedFadeTrigger.LowerBound), 0f, 1f); 
+			}
+		}	
+
+		return orig(self, player, mode);
+	}
+}
