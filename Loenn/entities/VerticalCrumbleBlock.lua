ce3335517c20429f9b07d55d5a92dfc6bcdea525
crckrbrrs <crackedberries@gmail.com>
2026-07-16 16:31:27 +0300

    these suck so bad

---
+++ Loenn/entities/VerticalCrumbleBlock.lua
@@ -0,0 +1,13 @@
+return {
+	name = "YaoiHelper/VerticalCrumbleBlock",
+	placements = {
+		name = "main",
+		data = {
+			width = 16,
+			height = 16,
+		}
+	},
+
+	-- TODO work out a proper texture for these at some point
+	color = {1, 1, 1, 0.2}
+}
